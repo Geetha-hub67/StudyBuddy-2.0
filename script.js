fe3b4d5d76498be76f1b0dffd3954b7a066e9048
Geetha Srinivasan <geethasrinivasan049@gmail.com>
2026-06-06 10:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function generateNotes(){
-  alert (" StudyBuddy 2.0 is working!");
+function generateNotes() {
+    let topic = document.querySelector("input").value;
+
+    if(topic === "") {
+        alert("Please enter a topic");
+    } else {
+        alert("Notes for: " + topic);
+    }
 }
